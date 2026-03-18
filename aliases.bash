2399fc1c504481dc6c6544ea7cf4493fee6ae6a8
aizuknight <aizuknight@outlook.jp>
2026-03-18 21:04:46 +0900

    Add aliases.bash and vim.conf

---
+++ aliases.bash
@@ -0,0 +1,5 @@
+if command -v kitten >/dev/null 2>&1; then
+  alias icat="kitten icat"
+  alias ssh="kitten ssh"
+fi
+
--- copy_current_config_to_repository.sh
+++ copy_current_config_to_repository.sh
@@ -1,4 +1,6 @@
 cp ~/.config/i3/config i3.conf
 cp ~/.config/i3status/config i3status.conf
 cp ~/.config/kitty/kitty.conf kitty.conf
+cp ~/.vimrc vim.conf
+cp ~/.bash_aliases aliases.bash
 
