ea0091f8fff68ecab571e08d1d06af095cb20d8c
aizuknight <aizuknight@outlook.jp>
2026-03-30 12:14:25 +0900

    Update aliases.bash, i3.conf and kitty.conf

--- aliases.bash
+++ aliases.bash
@@ -3,3 +3,9 @@ if command -v kitten >/dev/null 2>&1; then
   alias ssh="kitten ssh"
 fi
 
+if command -v bat > /dev/null 2>&1; then
+  alias bat="bat --paging=always"
+fi
+
+alias ll="ls -l"
+alias lla="ls -la"
